68e38e4afd8084a25304dbbdff00e6f953d57cbb
Abhishek Katta <VENKAT.GUGGILAM@gomomentus.com>
2023-08-17 21:09:46 +0530

    right rotation of an array

---
+++ ArrayRotation/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Arrayrotaion 
+{
+  class Program
+  {
+
+    static int[] rev_array(int[] array,int n)
+    {
+      int length = array.Length;
+      int[] rev_ar = new int[length];
+      for (int i =0; i < length; i++) 
+      {
+        int index = (i - n + length) % length;
+        rev_ar[i] = array[index]; 
+      }
+      return rev_ar;
+    }
+    static void Main(string[] args) 
+    {
+      Console.WriteLine("Enter the number of elements to be in a array");
+      int n = int.Parse(Console.ReadLine());
+      Console.WriteLine("No of times to rotate: ");
+      int no_of_rotations = int.Parse(Console.ReadLine());  
+
+      int[] array_a = new int[n];
+
+      for (int i = 0; i < n; i++)
+      {
+        Console.Write("Enter number: ");
+        array_a[i] = int.Parse(Console.ReadLine());
+      }
+      int[] result = rev_array(array_a, no_of_rotations);
+      foreach (int item in result) 
+      {
+        Console.WriteLine(item + " ") ;
+      }
+    }
+  }
+}
---
+++ Unique Elements/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace uniqueValues 
+{
+  class Program
+  {
+    static int[] originalArray(int[] array) 
+    {
+      HashSet<int> unique_values = new HashSet<int>(array);
+      return unique_values.ToArray();
+    }
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Enter the number of elements to be in a array");
+      int n = int.Parse(Console.ReadLine());
+      
+      int[] array_a = new int[n];
+
+      for (int i = 0; i < n; i++) 
+      {
+        Console.Write("Enter number: ");
+        array_a[i] = int.Parse(Console.ReadLine());
+      }
+
+      Console.Write("unique values are : ");
+      
+      int[] result = originalArray(array_a);
+      foreach (int item in result) 
+      {
+        Console.Write(item + " ");
+      }
+      
+    }
+  }
+}
+
